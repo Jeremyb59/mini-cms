df57d03c20164a4e39d117baa848b2b5992d000d
Jérémy Bouillet <88490357+Jeremyb59@users.noreply.github.com>
2023-05-19 22:53:59 +0200

    Update SongsController.php

--- application/Controller/SongsController.php
+++ application/Controller/SongsController.php
@@ -15,38 +15,16 @@ class SongsController
         require APP . 'view/songs/index.php';
         require APP . 'view/_templates/footer.php';
     }
-
-    /**
-     * ACTION: addSong
-     * This method handles what happens when you move to http://yourproject/songs/addsong
-     * IMPORTANT: This is not a normal page, it's an ACTION. This is where the "add a song" form on songs/index
-     * directs the user after the form submit. This method handles all the POST data from the form and then redirects
-     * the user back to songs/index via the last line: header(...)
-     * This is an example of how to handle a POST request.
-     */
+    
     public function addSong()
     {
-        // if we have POST data to create a new song entry
         if (isset($_POST["submit_add_song"])) {
-            // Instance new Model (Song)
             $Song = new Song();
-            // do addSong() in model/model.php
-            $Song->addSong($_POST["artist"], $_POST["track"],  $_POST["link"]);
+            $Song->addSong($_POST["artist"], $_POST["track"], $_POST["link"]);
         }
-
-        // where to go after song has been added
         header('location: ' . URL . 'songs/index');
     }
 
-    /**
-     * ACTION: deleteSong
-     * This method handles what happens when you move to http://yourproject/songs/deletesong
-     * IMPORTANT: This is not a normal page, it's an ACTION. This is where the "delete a song" button on songs/index
-     * directs the user after the click. This method handles all the data from the GET request (in the URL!) and then
-     * redirects the user back to songs/index via the last line: header(...)
-     * This is an example of how to handle a GET request.
-     * @param int $song_id Id of the to-delete song
-     */
     public function deleteSong($song_id)
     {
         // if we have an id of a song that should be deleted
